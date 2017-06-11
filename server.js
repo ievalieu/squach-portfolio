04cf00fa5fa9ae23ca4da10b265a0b8d5b375a9e
Eva Lieu <itsevalieu@gmail.com>
2017-06-11 14:16:25 -0700

    Fixing routes and handlebars views

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+//Dependencies
+//=========================================================
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+//var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
-var expressValidator = require("express-validator");
-var flash = require("connect-flash");
-var session = require("express-session");
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
+//var expressValidator = require("express-validator");
+//var flash = require("connect-flash");
+//var session = require("express-session");
+//var passport = require("passport");
+//var LocalStrategy = require("passport-local").Strategy;
 var mongojs = require("mongojs");
 var mongoose = require("mongoose");
 
+//Set up Mongo Connection
+//=========================================================
 mongoose.connect("mongodb://localhost/squach-portfolio");
 var db = mongoose.connection;
 
@@ -21,70 +25,83 @@ db.once("open", function() {
 	console.log("Mongoose connection successful.");
 });
 
+//Routes
+//=========================================================
 var routes = require("./server/controllers/index");
-var user = require("./server/controllers/user");
+// var user = require("./server/controllers/user");
 
 //Init App
+//=========================================================
 var app = express();
 
-//View Engine
-app.set("views", path.join(__dirname, "views"));
+//View Engine for Handlebars
+//=========================================================
+app.set("views", path.join(__dirname, "views/layouts"));
 app.engine("handlebars", exphbs({defaultLayout: "index"}));
 app.set("view engine", "handlebars");
 
 //BodyParser Middleware
+//=========================================================
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 //Set Static Folder
+//=========================================================
 app.use(express.static(path.join(__dirname,"public")));
 
 //Express Session
-app.use(session({
-	secret: "secret",
-	saveUninitialized: true,
-	resave: true
-}));
+//=========================================================
+// app.use(session({
+// 	secret: "secret",
+// 	saveUninitialized: true,
+// 	resave: true
+// }));
 
 //Passport Init
-app.use(passport.initialize());
-app.use(passport.session());
+//=========================================================
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //Express Validator
-app.use(expressValidator({
-	errorFormatter: function(param, msg, value) {
-		var namespace = param.split(".")
-		, root = namespace.shift()
-		, formParam = root;
-		while(namespace.length) {
-			formParam += "[" + namespace.shift() + "]";
-		}
-		return {
-			param: formParam,
-			msg: msg,
-			value: value
-		};
-	}
-}));
+//=========================================================
+// app.use(expressValidator({
+// 	errorFormatter: function(param, msg, value) {
+// 		var namespace = param.split(".")
+// 		, root = namespace.shift()
+// 		, formParam = root;
+// 		while(namespace.length) {
+// 			formParam += "[" + namespace.shift() + "]";
+// 		}
+// 		return {
+// 			param: formParam,
+// 			msg: msg,
+// 			value: value
+// 		};
+// 	}
+// }));
 
 //Connect Flash
-app.use(flash());
+//=========================================================
+//app.use(flash());
 
 //Global Variables
-app.use(function(req, res, next) {
-	res.locals.success_msg = req.flash("success_msg");
-	res.locals.error_msg = req.flash("error_msg");
-	res.locals.error = req.flash("error");
-	res.locals.user = req.user || null;
-	next();
-});
+//=========================================================
+// app.use(function(req, res, next) {
+// 	res.locals.success_msg = req.flash("success_msg");
+// 	res.locals.error_msg = req.flash("error_msg");
+// 	res.locals.error = req.flash("error");
+// 	res.locals.user = req.user || null;
+// 	next();
+// });
 
 //Middleware for Routes
+//=========================================================
 app.use("/", routes);
-app.use("/user", user);
+// app.use("/user", user);
 
 //Set Port
+//=========================================================
 app.set("port", (process.env.PORT || 3000));
 app.listen(app.get("port"), function(){
 	console.log("Server started on port " + app.get("port"));
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,27 +1,32 @@
 var express = require("express");
 var router = express.Router();
 
-//Get Homepage
-router.get("/", ensureAuthenticated, function(req, res){
-	res.render("index", {layout:"portfolio.handlebars"});
-});
-router.get("/portfolio", ensureAuthenticated, function(req, res){
-	res.render("portfolio", {layout:"portfolio.handlebars"});
-});
-router.get("/contact", ensureAuthenticated, function(req, res){
-	res.render("contact", {layout:"contact.handlebars"});
-});
-router.get("/resume", ensureAuthenticated, function(req, res){
-	res.redirect("index");
-});
+// //Get Homepage
+// router.get("/", ensureAuthenticated, function(req, res){
+// 	res.render("index", {layout:"portfolio.handlebars"});
+// });
+// router.get("/portfolio", ensureAuthenticated, function(req, res){
+// 	res.render("portfolio", {layout:"portfolio.handlebars"});
+// });
+// router.get("/contact", ensureAuthenticated, function(req, res){
+// 	res.render("contact", {layout:"contact.handlebars"});
+// });
+// router.get("/resume", ensureAuthenticated, function(req, res){
+// 	res.redirect("index");
+// });
 
-function ensureAuthenticated(req, res, next) {
-	if(req.isAuthenticated()) {
-		return next();
-	} else {
-		req.flash("error_msg", "You are not logged in."); //can remove if want
-		res.redirect("/users/login");
-	}
-}
+// function ensureAuthenticated(req, res, next) {
+// 	if(req.isAuthenticated()) {
+// 		return next();
+// 	} else {
+// 		req.flash("error_msg", "You are not logged in."); //can remove if want
+// 		res.redirect("/users/login");
+// 		// res.redirect("/");
+// 	}
+// }
 
+
+router.get("/portfolio", function(req, res){
+ 	res.render("index", {layout:"portfolio.handlebars"});
+});
 module.exports = router;
