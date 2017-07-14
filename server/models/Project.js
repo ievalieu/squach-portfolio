7026df771e0dc3b157dc609f065a6d09d638364b
Eva Lieu <itsevalieu@gmail.com>
2017-07-13 23:33:45 -0700

    Project model, attributes

--- server/models/Project.js
+++ server/models/Project.js
@@ -4,7 +4,19 @@ var Schema = mongoose.Schema;
 
 //User Schema
 var ProjectSchema = new Schema({
-	
+	title: {
+		type: String,
+		index: true
+	},
+	description: {
+		type: String
+	},
+	tag: {
+		type: String
+	},
+	category: {
+		type: String
+	}
 });
 
 var Project = module.exports = mongoose.model("Project", ProjectSchema);
